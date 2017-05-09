15bb81082f95d61fce487e6b18ea8e6a36ae1480
tonygargiulo <tony@gargiulo.com>
2017-05-09 08:41:04 -0700

    heroku not working

--- server/server.js
+++ server/server.js
@@ -6,10 +6,9 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
-
-
-
 var app = express();
+const port = process.env.PORT || 3000;
+
 
 app.use(bodyParser.json());
 
@@ -75,7 +74,7 @@ app.get('/todos/:id', (req, res) => {
 
 
 app.listen(3000, () => {
-  console.log('Started on port 3000');
+  console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
