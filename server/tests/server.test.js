be839c45b4d0548f2cc14d149157443d6bcb058c
tonygargiulo <tony@gargiulo.com>
2017-05-08 20:52:31 -0700

    Add test cases for GET \/todos\/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+  _id: new ObjectID(),
   text: 'First test todo'
 }, {
+  id: new ObjectID(),
   text: 'Second test todo'
 }];
 
@@ -71,4 +74,32 @@ describe('Get /todos', () => {
         })
         .end(done);
     });
+});
+
+describe('GET /todos/:id', () => {
+  it('should return todo doc', (done) => {
+    request(app)
+    .get(`/todos/${todos[0]._id.toHexString()}`)
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todo.text).toBe(todos[0].text);
+    })
+    .end(done);
+  });
+
+  it('should return a 404 if todo not found', (done) => {
+    id = new ObjectID().toHexString();
+    request(app)
+      .get(`/todos/${id}`) //request a valid id not in db
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 for non-object ids', (done) => {
+    // /todos/1234
+    request(app)
+      .get(`/todos/1234`) // request invalid id wrong format
+      .expect(404)
+      .end(done);
+  });
 });
